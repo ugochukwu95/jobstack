43bd69a3512417c84967df64cb6c508fd02fde1a
Ugochukwu Oguejiofor <54842706+ugochukwu95@users.noreply.github.com>
2019-09-03 12:15:55 +0100

    Add files via upload

---
+++ include/config.php
@@ -0,0 +1,68 @@
+<?php
+// SITE_ROOT contains the full path to the jobstack folder
+define('SITE_ROOT', dirname(dirname(__FILE__)));
+
+// Application directories
+define('PRESENTATION_DIR', SITE_ROOT . '/presentation/');
+define('BUSINESS_DIR', SITE_ROOT . '/business/');
+define('TEMPLATE_DIR', SITE_ROOT . '/presentation/templates/');
+
+// Server HTTP port (can omit if the default 80 is used)
+define('HTTP_SERVER_PORT', '80');
+/* Name of the virtual directory the site runs in, for example: '/jobstack/' if the site runs at http://www.example.com/jobstack/
+'/' if the site runs at http://www.example.com/ */
+define('VIRTUAL_LOCATION', '/');
+
+// These should be true while developing the web site
+define('IS_WARNING_FATAL', false);
+define('DEBUGGING', false);
+
+// The error types to be reported
+define('ERROR_TYPES', E_ALL);
+
+// Settings about mailing the error messages to admin
+define('SEND_ERROR_MAIL', true);
+define('ADMIN_ERROR_MAIL', 'ugo.oguejiofor95@gmail.com');
+define('SENDMAIL_FROM', 'Errors@jobstack.com.ng');
+ini_set('sendmail_from', SENDMAIL_FROM);
+
+// By default we don't log errors to a file
+define('LOG_ERRORS', false);
+define('LOG_ERRORS_FILE', SITE_ROOT . '/errors_log.txt'); // Windows
+/* Generic error message to be displayed instead of debug info (when DEBUGGING is false) */
+define('SITE_GENERIC_ERROR_MESSAGE', '<h1>Jobstack Error!</h1>');
+
+// Database connectivity setup
+define('DB_PERSISTENCY', 'true');
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'jobstack_jobstack');
+define('DB_PASSWORD', 'Franc1smary@');
+define('DB_DATABASE', 'jobstack_jobstack');
+define('PDO_DSN', 'mysql:host=' . DB_SERVER . ';dbname=' . DB_DATABASE);
+
+// Administrator login information
+define('ADMIN_USERNAME', 'adminjobstackkingugo');
+define('ADMIN_PASSWORD', 'franc1smary0guej10f0r');
+
+define('SHORT_JOB_DESCRIPTION_LENGTH', 250);
+define('SHORT_ADMIN_POSTS_DESCRIPTION_LENGTH', 30);
+define('JOBS_PER_PAGE', 30);
+define('ADMIN_JOBS_PER_PAGE', 20);
+define('FRONT_PAGE_JOBS', 30);
+define('POSTS_PER_PAGE', 100);
+define('FRONT_PAGE_POSTS', 4);
+define('SIMILAR_JOBS_AMOUNT', 6);
+define('REVIEWS_PER_PAGE', 3);
+define('RECOMMENDED_JOBS_PER_PAGE', 10);
+define('HIGHEST_RECRUITING_LOCATIONS_COUNT', 50);
+define('HIGHEST_RECRUITING_COMPANIES_COUNT', 50);
+
+define('FT_MIN_WORD_LEN', 3);
+
+// Cart actions
+define('ADD_JOB', 1);
+define('REMOVE_JOB', 2);
+
+// Random value used for hashing
+define('HASH_PREFIX', 'K1-');
+?>
---
+++ include/site_title.php
@@ -0,0 +1,9 @@
+<?php
+	class SiteTitle {
+		public $mSiteTitle;
+
+		public function __construct() {
+			$this->mSiteTitle = 'Job Search | Jobstack';
+		}
+	}
+?>
